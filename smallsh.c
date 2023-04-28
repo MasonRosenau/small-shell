6461fe4c223d7fe4a074af53dfb8693ebbb8055e
your-github-username <rosenaumason@gmail.com>
2023-04-28 11:54:54 -0700

    Take user input on repeat

--- smallsh.c
+++ smallsh.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main() {
+    char* input = NULL;
+    size_t len = 0;
+    ssize_t read;
 
+    while(1){
+        
+        printf(": ");
+        fflush(stdout);
+        fflush(stdin);
+        //write to input, len is length of user input (including \n), and read is dynamic buffer size
+        read = getline(&input, &len, stdin);
+        printf("%s", input);
+        printf("len: %d\n", read);
+        printf("read: %d\n", len);
+    
+    }
 
+    free(input);
     return 0;
-
 }
